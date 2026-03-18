f4205910d03ab5c454b76208c67ed4de55c79be6
justlinux <ppeter.brilla@gmail.com>
2026-03-18 16:38:35 +0100

    Added conform.nvim formatter to neovim

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = 'master',
-  lazy = false,
   build = ":TSUpdate",
   config = function()
     local config = require('nvim-treesitter.configs')
